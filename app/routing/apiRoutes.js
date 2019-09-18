d88af25ba8e194d1722b8127e6aad37df2eb2f79
Edel Marcelino <edelm8@gmail.com>
2019-09-17 23:12:52 -0700

    added survey page

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,9 +4,11 @@ var path = require("path");
 var friendFinder = require("../data/friends.js");
 
 var app = express();
+
 var PORT = 3000;
 
 // Sets up the Express app to handle data parsing
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
