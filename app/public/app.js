4925ff12a31d6ee4cef3be28fda0cf9124c3f591
edelBCS <edelm8@gmail.com>
2019-09-19 21:16:22 -0700

    added modal

--- app/public/app.js
+++ app/public/app.js
@@ -1,56 +1,63 @@
-$("#submitBtn").on("click", (event) => {
-    event.preventDefault();
+$(document).ready(function(){
+    $("#missingData").hide();
 
+    $("#submitBtn").on("click", (event) => {
+        event.preventDefault();
     
-
-    //validate data
-    function validData() {
-        var isValid = true;
-
-        if($("#nameInput").val() === ""){
-            console.log("Missing Name!");
-            isValid = false;
-        }
-
-        if($("#photoLinkInput").val() === ""){
-            console.log("Missing Photo Link!");
-            isValid = false;
-        }
-
-        $(".question").each(function() {
-            console.log($(this).val())
-            if($(this).val() === "Choose..."){
-                console.log("Missing Question!");
+        
+    
+        //validate data
+        function validData() {
+            var isValid = true;
+    
+            if($("#nameInput").val() === ""){
                 isValid = false;
             }
-        });
     
-        return isValid;
-    }
+            if($("#photoLinkInput").val() === ""){
+                isValid = false;
+            }
+    
+            $(".question").each(function() {
+                if($(this).val() === "Choose..."){
+                    isValid = false;
+                }
+            });
+    
+            if(!isValid){
+                $("#missingData").show();
+            }
+        
+            return isValid;
+        }
+    
+        //post data to server and get back person
+        if(validData()){
+            var newFriend = {
+                name: $("#nameInput").val(),
+                photo: $("#photoLinkInput").val(),
+                scores: [
+                    parseInt($("#question1").val().charAt(0)),
+                    parseInt($("#question2").val().charAt(0)),
+                    parseInt($("#question3").val().charAt(0)),
+                    parseInt($("#question4").val().charAt(0)),
+                    parseInt($("#question5").val().charAt(0)),
+                    parseInt($("#question6").val().charAt(0)),
+                    parseInt($("#question7").val().charAt(0)),
+                    parseInt($("#question8").val().charAt(0)),
+                    parseInt($("#question9").val().charAt(0)),
+                    parseInt($("#question10").val().charAt(0))
+                ]
+            };
 
-    //post data to server and get back person
-    if(validData()){
-        var newFriend = {
-            name: $("#nameInput").val(),
-            photo: $("#photoLinkInput").val(),
-            scores: [
-                parseInt($("#question1").val().charAt(0)),
-                parseInt($("#question2").val().charAt(0)),
-                parseInt($("#question3").val().charAt(0)),
-                parseInt($("#question4").val().charAt(0)),
-                parseInt($("#question5").val().charAt(0)),
-                parseInt($("#question6").val().charAt(0)),
-                parseInt($("#question7").val().charAt(0)),
-                parseInt($("#question8").val().charAt(0)),
-                parseInt($("#question9").val().charAt(0)),
-                parseInt($("#question10").val().charAt(0))
-            ]
-        };
-        alert(newFriend.name + newFriend.photo + newFriend.scores)
-        $.post("/api/friends", newFriend, (data) => {
-            console.log(data);
-        });
-    }
+            $.post("/api/friends", newFriend, (data) => {
+                $("#exampleModalCenter").modal("toggle");
+                console.log(data);
+            });
+        }
+    });
 });
+
+
 
 
